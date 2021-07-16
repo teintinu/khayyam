3706fb5770efc17b7669169fa184be1d0879df01
teintinu <JoseFernandoTolentino@gmail.com>
2021-07-16 13:13:48 +0000

    first monoclean version

---
+++ scripts/prepare.js
@@ -0,0 +1,15 @@
+
+const fs = require('fs')
+
+const github_ref = process.env.GITHUB_REF || ''
+const packageJsonFile = process.argv[2]
+
+const matchesTag = /^refs\/tags\/v(.*)$/.exec(github_ref)
+
+if (matchesTag && matchesTag[ 1 ]) {
+  const version = matchesTag[ 1 ]
+  const packageJson = JSON.parse(fs.readFileSync(packageJsonFile, 'utf8'))
+  packageJson.version = version
+  console.log(JSON.stringify(packageJson, null, 2))
+  fs.writeFileSync(packageJsonFile, JSON.stringify(packageJson, null, 2), 'utf8')
+}
